0eb33973885ef7a18d7e20e74fef7f6ee7003f42
wmsxsd <dg2026626@danggok.hs.kr>
2026-05-28 08:55:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -64,8 +64,8 @@ while 1:
         #     dddd = 0
         if f1l.pos.x >= 8.25:
             f1l.pos.x = f1l.pos.x +1
-        if f1r.pos.x >= -1.75:
-           f1r.pos.x = f1r.pos.x -1
+        if f1r.pos.x <= -1.75:
+            f1r.pos.x = f1r.pos.x -1
         f1l.pos.x = f1l.pos.x -1
         f1r.pos.x = f1r.pos.x +1
        
